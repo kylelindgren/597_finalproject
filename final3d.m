9cd3ef924e18f4aa2d28290f3fa5cf13b5664218
kylelindgren <kylelindgren@hotmail.com>
2016-05-27 16:39:45 -0700

    done?

--- final3d.m
+++ final3d.m
@@ -11,51 +11,20 @@ e = 5; % error
 sigma = 1;
 distance = 0;
 bounds = 200; % 200x200
-%[L,iter,x0] = genConnectLap3d(n,bounds,V);
-%% 
-se = 1; ce = 0; % bools for sensor, control error existence
-[x_,y_,z_,phases,L] = finalsim3d(x0,L,N,sigma,V,e,se,ce);
+%[L,iter,x0] = genConnectLap3d(n,bounds,V); % approx 350k iterations so do
+% once and save 
+x0 = csvread('final3d_x0.csv');
+L  = csvread('final3d_L.csv');
+se = 0; ce = 0; % bools for sensor, control error existence
+[x_,y_,z_,phases,L] = final3dsim(x0,L,N,sigma,V,e,se,ce);
 for i=1:n
     distance = distance + norm([x_(i,1)-x_(i,end) y_(i,1)-y_(i,end) z_(i,1)-z_(i,end)]);
 end
-% csvwrite('fig5_phases_se5_n10_x.csv',x_',1,1);
-% csvwrite('fig5_phases_se5_n10_y.csv',y_',1,1);
-
-%plot(x_,y_)
+pt1 = round(0.25*phases); pt2 = round(0.5*phases); pt3 = round(0.75*phases);
 scatter3(x0(:,1),x0(:,2),x0(:,3),'ko','LineWidth',2)
-% hold on
-% scatter3(x_(:,end),y_(:,end),z_(:,end),'ro','LineWidth',2,'MarkerSize',5)
+hold on
+scatter3(x_(:,pt1),y_(:,pt1),z_(:,pt1),'b*','LineWidth',2)
+scatter3(x_(:,pt2),y_(:,pt2),z_(:,pt2),'g+','LineWidth',2)
+scatter3(x_(:,pt3),y_(:,pt3),z_(:,pt3),'mx','LineWidth',2)
+scatter3(x_(:,end),y_(:,end),z_(:,end),'ro','LineWidth',2)
 axis([0 bounds 0 bounds 0 bounds]);
-
-% 
-% B = 1.0;
-% [x1_,t_] = sim92(t,x0,L,B);
-% B = 0.6;
-% [x2_,t_] = sim92(t,x0,L,B);
-% B = 0.4;
-% [x3_,t_] = sim92(t,x0,L,B);
-% 
-% subplot(3,1,1)
-% hold on
-% %Plot to end
-% plot(st,x1_);
-% legend('p1 pos', 'p2 pos', 'p3 pos', 'p4 pos', 'p5 pos','location', 'southeast');
-% %Plot initial positions
-% plot(0,x0(:,1),'ko','LineWidth',2,'MarkerSize',10)
-% title('hw7 problem 9.2: Star Graph Max-Protocol with Beta = 1.0');
-% ylabel('position');
-% 
-% subplot(3,1,2)
-% hold on
-% plot(st,x2_);
-% plot(0,x0(:,1),'ko','LineWidth',2,'MarkerSize',10)
-% title('Beta = 0.6');
-% ylabel('position');
-% 
-% subplot(3,1,3)
-% hold on
-% plot(st,x3_);
-% plot(0,x0(:,1),'ko','LineWidth',2,'MarkerSize',10)
-% title('Beta = 0.4');
-% xlabel('time');
-% ylabel('position');
--- final3dsim.m
+++ final3dsim.m
@@ -93,23 +93,37 @@ function [x_,y_,z_,j,L] = final3dsim(x0,L,N,sigma,V,e,se,ce)
             for p=1:n
                 L(p,p) = abs(sum(L(p,:)<0,2));
             end
-            %%
+            %
             for k=1:n
                 if L(i,k) < 0 % connected
                     ndist  = norm([x_(i,j)-x_(k,j), y_(i,j)-y_(k,j), z_(i,j)-z_(k,j)]);
                     ndir = [x_(k,j)-x_(i,j), y_(k,j)-y_(i,j), z_(k,j)-z_(i,j)];
-                    %ntheta = atan2(y_(k,j)-y_(i,j), x_(k,j)-x_(i,j));
                     if se % add sensing error
                         derr   = -e + 2*e*rand(1); 
                         terr   = [-e + 2*e*rand(1), -e + 2*e*rand(1), -e + 2*e*rand(1)]; % theta error
-                        ndist  = (1+0.01*derr)*ndist;
                         ndir = terr*ndist*0.01 + ndir;
+                        ndist  = (1+0.01*derr)*ndist;
                     end
                     circle_pts(k,:) = ndist.*ndir + [x_(i,j) y_(i,j) z_(i,j)];
                 end
             end
             circle_pts(~any(circle_pts,2),:) = []; % remove zero rows
-            [center,radius,Xb] = ExactMinBoundSphere3D([circle_pts(:,1),circle_pts(:,2),circle_pts(:,3)]);
+            if size(circle_pts,1) > 3
+                [radius,center,Xb] = ExactMinBoundSphere3D([circle_pts(:,1),circle_pts(:,2),circle_pts(:,3)]);
+            else % cannot create convex hull with less than 3 points, approx center, radius
+                center = [0 0 0];
+                for d = 1:size(circle_pts,1)
+                    center = center + circle_pts(d,:);
+                end
+                radius = 0;
+                for d = 1:size(circle_pts,1)
+                    temp = norm(center - circle_pts(d,:));
+                    if temp > radius
+                        radius = temp;
+                    end
+                end
+                center = center./size(circle_pts,1);
+            end
             goal_step = [(center(1,1)-x_(i,j)) (center(1,2)-y_(i,j)) (center(1,3)-z_(i,j))];
             if norm(goal_step) == 0 % divide by zero check
                 norm_step = [0 0 0];
@@ -120,9 +134,12 @@ function [x_,y_,z_,j,L] = final3dsim(x0,L,N,sigma,V,e,se,ce)
             if radius < 3*sqrt(n/2)
                 min = 0;
             else
+                line = [x_(i,j) y_(i,j) z_(i,j) norm_step(1) norm_step(2) norm_step(3)]; % x0 and direction of a line
                 for s=1:size(circle_pts,1)
-                    dist = norm([(x_(i,j)-circle_pts(s,1)) (y_(i,j)-circle_pts(s,2)) (z_(i,j)-circle_pts(s,3))]);
-                    length = 0.5*dist*norm_step(1,1) + sqrt((0.5*V)^2 - (0.5*dist*norm_step(1,2))^2);
+                    midpoint = 0.5*[(x_(i,j)+circle_pts(s,1)) (y_(i,j)+circle_pts(s,2)) (z_(i,j)+circle_pts(s,3))];
+                    sphere = [midpoint(1) midpoint(2) midpoint(3) 0.5*V]; % sphere center and radius
+                    points = intersectLineSphere(line, sphere); % intersection point of line between i and center and sphere surface
+                    length = norm(points(1,:) - [x_(i,j) y_(i,j) z_(i,j)]);
                     if length < min
                         min = length;
                     end
@@ -141,31 +158,37 @@ function [x_,y_,z_,j,L] = final3dsim(x0,L,N,sigma,V,e,se,ce)
             % check for collision (no error added yet)
             x_(i,j+1) = x_(i,j) + step(1);
             y_(i,j+1) = y_(i,j) + step(2);
+            z_(i,j+1) = z_(i,j) + step(3);
             swerve = 0; % flag to only swerve once
             for r=1:n
                 if r ~= i && ~swerve 
-                    if norm([x_(i,j+1)-x_(r,j), y_(i,j+1)-y_(r,j)]) <= 1
+                    if norm([x_(i,j+1)-x_(r,j), y_(i,j+1)-y_(r,j), z_(i,j+1)-z_(r,j)]) <= 1
                         swerve = 1; % swerve right, decrease speed by 0.5
                         theta = atan2(step(2),step(1)) - 0.5*pi;
                         dist  = norm(step)*0.5*sigma;
-                        step  = dist.*[cos(theta) sin(theta)];
+                        step  = dist.*[cos(theta) sin(theta) 0] + [0 0 step(3)]; % z unchanged
                     end
                 end
             end
             cdist  = norm(step);
-            ctheta = atan2(step(2),step(1));
+            if cdist == 0
+                cdir = [0 0 0];
+            else
+                cdir = step./cdist;
+            end
             if ce % add control error
                 derr   = -e + 2*e*rand(1); 
-                terr   = (-e + 2*e*rand(1))*pi/180; % theta error in radians
+                terr   = [-e + 2*e*rand(1), -e + 2*e*rand(1), -e + 2*e*rand(1)];
+                cdir = terr*cdist*0.01 + cdir;
                 cdist  = (1+0.01*derr)*cdist;
-                ctheta = terr + ctheta;
             end
-            step = cdist.*[cos(ctheta) sin(ctheta)];
+            step = cdist.*cdir;
 
             x_(i,j+1) = x_(i,j) + step(1);
             y_(i,j+1) = y_(i,j) + step(2);
+            z_(i,j+1) = z_(i,j) + step(3);
         end
-        if prod(round(100*(x_(:,j+1)-x_(:,j))) == zeros(n,1)) && prod(round(100*(y_(:,j+1)-y_(:,j))) == zeros(n,1))
+        if prod(round(10*(x_(:,j+1)-x_(:,j))) == zeros(n,1)) && prod(round(10*(y_(:,j+1)-y_(:,j))) == zeros(n,1)) && prod(round(10*(z_(:,j+1)-z_(:,j))) == zeros(n,1))
             done = 1; j = j-1; % finished one iteration ago
         end
         j = j+1;
